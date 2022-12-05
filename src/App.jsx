d4ac8423fe9f4de7a1390b1c85b086c593ae31b2
Kononov <akellastoopi@gmail.com>
2022-12-05 17:55:29 +0200

    fix fetching logic from postgre

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 // @ts-nocheck
 import "./App.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 
 function App() {
 	const [data, setData] = useState(null);
+	const [id, setId] = useState(null);
+	const [isFetching, setIsFetching] = useState(false);
 	const [language, setLanguage] = useState("javascript");
 	const ref = useRef(null);
 	const url = `http://localhost:8000`;
 
 	const compileHandler = async () => {
 		try {
-			const { data } = await axios.post(
+			const {
+				data: { id },
+			} = await axios.post(
 				url,
 				{ code: ref.current.value, language: language },
 				{
@@ -20,11 +24,39 @@ function App() {
 					},
 				}
 			);
-			setData(data);
+			setId(id);
+			setIsFetching(true);
 		} catch (error) {
 			console.log(error);
 		}
 	};
+	useEffect(() => {
+		if (!isFetching && !id) {
+			return;
+		}
+		const fetchHandler = async () => {
+			try {
+				const res = await axios.get(`${url}/${id}`);
+				let { result } = res.data.result[0];
+
+				if (res) {
+					setIsFetching(false);
+					setId(null);
+					setData(result);
+				} else {
+					console.log("No response from the server yet");
+				}
+			} catch (error) {
+				throw error;
+			}
+		};
+
+		let interval = setInterval(fetchHandler, 5000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [isFetching, id]);
 
 	return (
 		<div className="app">
